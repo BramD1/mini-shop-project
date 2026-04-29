3bf5d18b605e123bd18192cf1de1f303a617d687
Bramantyo Anandaru Suyadi <daru3317@gmail.com>
2026-04-29 20:47:32 +0700

    v1 toko repo is finished

--- repository/toko_repository.go
+++ repository/toko_repository.go
@@ -1,2 +1,51 @@
 package repository
 
+import (
+    "mini-shop-project/domain"
+    "gorm.io/gorm"
+)
+
+type tokoRepository struct {
+    db *gorm.DB
+}
+
+func NewTokoRepository(db *gorm.DB) domain.TokoRepository {
+    return &tokoRepository{db: db}
+}
+
+func (r *tokoRepository) FindAll(limit, offset int, nama string) ([]domain.Toko, error) {
+    var tokos []domain.Toko
+    query := r.db
+    if nama != "" {
+        query = query.Where("nama_toko LIKE ?", "%"+nama+"%")
+    }
+    err := query.Limit(limit).Offset(offset).Find(&tokos).Error
+    return tokos, err
+}
+
+func (r *tokoRepository) FindByID(id uint) (domain.Toko, error) {
+    var toko domain.Toko
+    err := r.db.First(&toko, id).Error
+    return toko, err
+}
+
+func (r *tokoRepository) FindByUserID(userID uint) (domain.Toko, error) {
+    var toko domain.Toko
+    err := r.db.Where("user_id = ?", userID).First(&toko).Error
+    return toko, err
+}
+
+func (r *tokoRepository) Create(toko domain.Toko) (domain.Toko, error) {
+    err := r.db.Create(&toko).Error
+    return toko, err
+}
+
+func (r *tokoRepository) Update(toko domain.Toko) (domain.Toko, error) {
+    err := r.db.Save(&toko).Error
+    return toko, err
+}
+
+func (r *tokoRepository) Delete(id uint) error {
+    err := r.db.Delete(&domain.Toko{}, id).Error
+    return err
+}
